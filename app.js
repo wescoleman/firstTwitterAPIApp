8f9604673633d7c7ed96a5f7d385750b848bf09f
Wesley Coleman <38671275+wescoleman@users.noreply.github.com>
2021-09-26 21:09:21 -0400

    Added Geographically specific queries
    used geo specific queries to search in an area (boston) about tweets concerning the redsox.
    
    Included a picture of the browser output on localhost

--- app.js
+++ app.js
@@ -2,12 +2,15 @@ var Twit = require('twit'),
     fs = require('fs'),
     express = require("express"),
     app = express(),
-    toDisplay;
+    soxTweets,
+    tweets;
 
 
 require('dotenv').config();
 
 app.use(express.static('public'));
+
+
  
 var T = new Twit({
   consumer_key:         process.env.TWITTER_API_KEY,
@@ -21,8 +24,10 @@ var T = new Twit({
 
 
 T.get('search/tweets', { q: 'Rihanna since:2021-09-25', count: 10 }, function(err, data, response) {
-  console.log(data);
-  toDisplay = JSON.stringify(data.statuses.text, null, "\t");
+  // console.log(data);
+  for (var i = data.statuses.length - 1; i >= 0; i--) {
+      tweets = tweets + JSON.stringify(data.statuses[i].text)+"<div></div><div></div>"
+  }
   fs.writeFile("rihanna.json", JSON.stringify(data, null, "\t"), function(err) {
     if (err) {
         console.log(err);
@@ -30,12 +35,16 @@ T.get('search/tweets', { q: 'Rihanna since:2021-09-25', count: 10 }, function(er
   });
 })
 
-
-
+T.get('search/tweets', { q: 'redsox since:2021-01-25', geocode: '42,-71,100mi',count: 10}, function(err, data, response) {
+  console.log(data);
+  for (var i = data.statuses.length - 1; i >= 0; i--) {
+      soxTweets = soxTweets + JSON.stringify(data.statuses[i].text)+"<div></div><div></div>"
+  }
+})
 
 
 app.get('/', function(req, res) {
-    res.send('There is a json file with tweets about rihanna in it');
+    res.send("<h1>RIHANNA TWEETS</h1><div></div>"+tweets+"<div></div><h1>GO SOX</h1>" +soxTweets);
 });
 
 app.listen(process.env.PORT || 1234, function() {
